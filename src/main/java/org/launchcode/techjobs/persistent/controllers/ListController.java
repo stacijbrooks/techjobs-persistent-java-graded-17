0f3af31663668b5b29288c0a9dc35090dd52b1b4
stacijbrooks <stacibrooksfitness@gmail.com>
2024-06-15 18:27:10 -0500

    Update List Controller

--- src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
@@ -47,7 +47,7 @@ public class ListController {
     public String list(Model model) {
         model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute("skills", skillRepository.findAll());
-        model.addAttribute("columnChoices", columnChoices);
+        //model.addAttribute("columnChoices", columnChoices);
 
         return "list";
     }
